8dfdec25d9249e7bef66ff388c2937ba47bde4c1
reactdattu2021 <react.dattu2021@gmail.com>
2025-07-19 19:21:14 +0530

    cleare

--- src/components/Hospitaladmin1/HospitalAdminDashboard.jsx
+++ src/components/Hospitaladmin1/HospitalAdminDashboard.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import RegisterHospitalForm from './RegisterHospitalForm';
 import ManageDepartments from './ManageDepartments';
 import HospitalReports from './HospitalReports';
-import SelectDropdown from '../Common/SelectDropdown';
+import SelectDropdown from '../Common/selectDropdown';
 
 const HospitalAdminDashboard = ({ hospitals, doctors, departments, appointments, addHospital, addDepartment, showAppMessage }) => {
     const [selectedHospitalForDept, setSelectedHospitalForDept] = useState('');
